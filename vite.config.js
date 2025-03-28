c154a4e9fda267c4360235c1eed75158f5964a00
eduniklas <niklas.hagelin@edu.edugrade.se>
2025-03-28 05:43:07 +0100

    more vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   resolve: {
     alias: {
       "@": path.resolve("./src"),
     },
   },
   assetsInclude: ['**/*.gltf', '**/*.glb', '**/*.bin'],
+  base: '/bottle-app/',
 })
